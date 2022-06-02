d6a9b139f3d55c395d8f0bfbc089d526c2ab15f1
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-06-02 16:54:11 -0500

    added my code

--- my_boggle.py
+++ my_boggle.py
@@ -14,16 +14,16 @@ class BoggleBoard:
         for row in self.board:
             strings.append(" ".join(self._char_map.get(char, char).ljust(2) for char in row))
         return "\n".join(strings)
-        
 
     def shake(self):
-        self.board = [[random.choice(ascii_uppercase) for i in range(4)] for i in range(4)]
+        self.board = [[random.choice(ascii_uppercase) for i in range(4)] for i_ in range(4)]
+
 
 def main():
     boggle = BoggleBoard()
     print(boggle)
     boggle.shake()
     print(boggle)
-
+###### Why do we need this ##########
 if __name__ =="__main__":
     main()
